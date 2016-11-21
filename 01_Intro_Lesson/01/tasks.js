873b36b225c5509389e309534bde7d2814774857
krambertech <k.porshnieva@gmail.com>
2016-11-21 20:05:32 +0200

    Add tasks comments from 1_1

---
+++ 01_Intro_Lesson/01/tasks.js
@@ -0,0 +1,96 @@
+/*
+ * #1
+ * Получить из данного массива пользователей
+ * массив их полных имен
+ * EX: ['Vasya Vasiliev', 'Ivan Ivanov']
+*/
+
+{
+    const users = [
+        { id: 1, name: 'Vasya', surname: 'Vasiliev' },
+        { id: 2, name: 'Ivan', surname: 'Ivanov' },
+        { id: 3, name: 'Irina', surname: 'Plushkina' }
+    ];
+
+
+}
+
+/*
+ * #2
+ * Получить из данного массива пользователей
+ * только тех, кто старше определенного возраста
+*/
+
+{
+    const users = [
+        { id: 1, age: 21, name: 'Vasya', surname: 'Vasiliev' },
+        { id: 2, age: 28, name: 'Ivan', surname: 'Ivanov' },
+        { id: 3, age: 18, name: 'Irina', surname: 'Plushkina' }
+    ];
+
+
+}
+
+/*
+ * #3
+ * Получить из данного массива заказов
+ * среднюю сумму заказа
+*/
+
+{
+    const orders = [{
+        id: 5,
+        date: '21-01-2015',
+        amount: 783
+    }, {
+        id: 8,
+        date: '24-01-2015',
+        amount: 67
+    }, {
+        id: 21,
+        date: '29-01-2015',
+        amount: 1234
+    }, {
+        id: 78,
+        date: '04-02-2015',
+        amount: 123
+    }, {
+        id: 23,
+        date: '15-02-2015',
+        amount: 245
+    }];
+
+
+}
+
+/*
+ * #3
+ * Получить из данного массива заказов
+ * среднюю сумму заказа
+*/
+
+{
+    const orders = [{
+        id: 5,
+        date: '21-01-2015',
+        amount: 783
+    }, {
+        id: 8,
+        date: '24-01-2015',
+        amount: 67
+    }, {
+        id: 21,
+        date: '29-01-2015',
+        amount: 1234
+    }, {
+        id: 78,
+        date: '04-02-2015',
+        amount: 123
+    }, {
+        id: 23,
+        date: '15-02-2015',
+        amount: 245
+    }];
+
+
+}
